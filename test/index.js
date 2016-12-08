34e1dcc54a64ddcc00a9e99d6bf1ecffe51e4585
Dan Benson <dan.benson@mdinsider.com>
2016-12-07 17:11:05 -0800

    basic tests

--- test/index.js
+++ test/index.js
@@ -1,12 +1,20 @@
-import http from 'http';
+import request from 'request';
 import assert from 'assert';
+import _ from 'lodash';
 
 import '../lib/index.js';
 
 describe('Bus Server', () => {
   it('should return 200', done => {
-    http.get('http://127.0.0.1:3000', res => {
+    request.get('http://127.0.0.1:3000/nexttimes', (err, res, body) => {
       assert.equal(200, res.statusCode);
+      done();
+    });
+  });
+  it('should return an array', done => {
+    request.get('http://127.0.0.1:3000/nexttimes', (err, res, body) => {
+      body = JSON.parse(body)
+      assert(_.isArray(body), true);
       done();
     });
   });
