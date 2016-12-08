0008434f8956d9c1a86dd62099be1559542158a2
Dan Benson <dan.benson@mdinsider.com>
2016-12-07 17:20:52 -0800

    freeze stop time logic

--- test/index.js
+++ test/index.js
@@ -1,6 +1,13 @@
 import request from 'request';
 import assert from 'assert';
 import _ from 'lodash';
+import moment from 'moment';
+
+const NUM_OF_STOPS = 10;
+const NUM_OF_ROUTES = 3;
+const STOP_MIN_OFFSET = 2;
+const ROUTE_MIN_OFFSET = 2;
+const BASE_INTERVAL = 15; //every minutes
 
 import '../lib/index.js';
 
@@ -18,4 +25,46 @@ describe('Bus Server', () => {
       done();
     });
   });
+  it('should return correctly formatted Bus Stop times', done => {
+    let now = Date.now();
+    request.get(`http://127.0.0.1:3000/nexttimes?time=${now}`, (err, res, body) => {
+      body = JSON.parse(body);
+      let expected = generateStopMap(now);
+      assert.deepEqual(body, expected);
+      done();
+    });
+  });
 });
+
+
+function nextTwoTimes(time, offset1, offset2) {
+  time = moment(parseInt(time, 10));
+  let totalOffset = (offset1 * STOP_MIN_OFFSET) + (offset2 * ROUTE_MIN_OFFSET);
+  let trainIsHere = (time.minute() % BASE_INTERVAL === totalOffset);
+  let trainHasPassed = (time.minute() % BASE_INTERVAL > totalOffset);
+
+  if (!trainIsHere) {
+    let lastStopMin = Math.round(time.minute() / BASE_INTERVAL) * BASE_INTERVAL + BASE_INTERVAL;
+    let nextStop = moment(time.hour(), 'hh').add(lastStopMin + totalOffset, 'minute').subtract(trainHasPassed ? 0 : BASE_INTERVAL, 'minute');
+    return [nextStop.format('hh:mm'),
+            nextStop.add(BASE_INTERVAL, 'minute').format('hh:mm')];
+  }
+  return ['now', time.add(BASE_INTERVAL, 'minutes').format('hh:mm')];
+}
+
+function generateStopMap(time) {
+  let stops = [];
+
+  for (let i=0; i<NUM_OF_STOPS; i++) {
+    stops.push({
+      stopId: i+1,
+      routes: _.times(NUM_OF_ROUTES, (index) => {
+        return {
+          routeId: index + 1,
+          nextTimes: nextTwoTimes(time, i, index)
+        };
+      })
+    });
+  }
+  return stops;
+}
